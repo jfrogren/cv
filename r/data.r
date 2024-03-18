5fa721972d7170edfda647c1c679308a5b8d57c3
joafr <joakim@frogren.se>
2024-03-18 12:26:06 +0100

    Lagt till en massa under övriga meriter

--- r/data.r
+++ r/data.r
@@ -26,14 +26,6 @@ skills1 <- tribble(
     "Italienska", "Grundläggande kunskaper"
 )
 
-skills3 <- tribble(
-    ~area, ~skills,
-    "Svenska", "Modersmål, talar och skriver flytande",
-    "Engelska", "Behärskar mycket väl i tal och skrift",
-    "Franska", "Behärskar väl i tal och skrift",
-    "Italienska", "Grundläggande kunskaper"
-)
-
 skills2 <- tribble(
   ~area, ~skills,
   "Microsoft Office", "Excel, Outlook, PowerPoint, Word — Behärskar väl",
@@ -43,6 +35,58 @@ skills2 <- tribble(
   "Språk", "R, RMarkdown, Markdown, CSS, HTML, YAML — Grundläggande kunskaper",
   "Textredigerare", "RStudio, Emacs — Grundläggande kunskaper",
   "Versionskontroll", "Git — Grundläggande kunskaper",
+)
+
+skills3 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Lunds universitet — koordinator", "Mars", 2018, "Pågående", NA,"SWEAH (Swedish National Graduate School on Ageing and Health)", "Lund", "Sedan 2018 medlem i SWEAH - en tvärvetenskaplig forskarskola inom åldrande och hälsa med 13 anslutna lärosäten",
+    "Lunds universitet — koordinator", "Mars", 2018, "Pågående", NA,"SWEAH (Swedish National Graduate School on Ageing and Health)", "Lund", "Forskarskolan erbjuder tvärvetenskapliga kurser inom åldrande och hälsa samt arrangerar aktiviteter för anslutna doktorander",
+    "Lunds universitet — koordinator", "Mars", 2018, "Pågående", NA,"SWEAH (Swedish National Graduate School on Ageing and Health)", "Lund", "Genomgångna kurser inom SWEAH: Gerontology (5 hp); Theories of aging (3 hp)",
+    "Lunds universitet — koordinator", "Mars", 2018, "Pågående", NA,"SWEAH (Swedish National Graduate School on Ageing and Health)", "Lund", "Aktivt deltagit i planeringen och genomförandet av SWEAH:s konferens i Lund (22-24 oktober, 2019)",
+    "Lunds universitet — koordinator", "Mars", 2018, "Pågående", NA,"SWEAH (Swedish National Graduate School on Ageing and Health)", "Lund", "Aktivt deltagit på studentdagar som anordnats två gånger per år, där tillfälle givits att presentera och ge återkoppling på forskning",
+)
+
+skills4 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Blekinge tekniska högskola", "Augusti", 2016, "Juni", 2018,"SMART4MD: Horizon 2020-projekt med fokus på att utfoma och utvärdera mobil applikation till stöd för personer med kognitiv svikt", "Karlskrona", "Ansvarig för planerande och genomförande av användbarhetsstudie med representanter ur målgruppen i syfte att utvärdera och utveckla appen",
+    "Blekinge tekniska högskola", "Augusti", 2016, "Juni", 2018,"SMART4MD: Horizon 2020-projekt med fokus på att utfoma och utvärdera mobil applikation till stöd för personer med kognitiv svikt", "Karlskrona", "På den svenska sajten ansvarig för att introducera appen till försöksdeltagarna samt finnas tillgänglig som teknisk support via mejl och telefon.",
+    "Blekinge tekniska högskola", "Augusti", 2016, "Juni", 2018,"SMART4MD: Horizon 2020-projekt med fokus på att utfoma och utvärdera mobil applikation till stöd för personer med kognitiv svikt", "Karlskrona", "En av huvudförfattarna till rapport till Europeiska kommissionen samt till två pedagogiska dokument, ett internt och ett som delades ut till studiedeltagare",
+)
+
+skills5 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Lunds universitet", "Augusti", 2018, "April", 2023,"UserAge: Sexårigt forskningsprogram med fokus på brukarinvolvering i forskning om åldrande och hälsa", "Lund", "Aktivt deltagit på månatlig webb-baserad seminarieserie tillsammans med doktorandkollegor och forskare vid de fyra lärosätena anslutna till forskningsprogrammet (Högskolan Kristianstad; Göteborgs universitet; Linnéuniversitetet; Lunds universitet)",
+    "Lunds universitet", "Augusti", 2018, "April", 2023,"UserAge: Sexårigt forskningsprogram med fokus på brukarinvolvering i forskning om åldrande och hälsa", "Lund", "Aktivt deltagit i dialogmöten och workshops med doktorandkollegor under vägledning av seniora forskare inom UserAge (2-3 gånger per termin under doktorandtiden)",
+    "Lunds universitet", "Augusti", 2018, "April", 2023,"UserAge: Sexårigt forskningsprogram med fokus på brukarinvolvering i forskning om åldrande och hälsa", "Lund", "Deltagit i ett flertal 'retreats' (online och IRL) där forskare från programmets fyra lärosäten mötts och diskuterat vad som åstadkommits hittills och vägar framåt",
+)
+
+skills6 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Lunds universitet", "Augusti", 2018, "April", 2020,"Beslutsstöd Bo Tillgängligt (BBT): Tvåårigt forskningsprojekt om förbättrad tillgänglighet i flerfamiljshus", "Lund", "Aktivt deltagit i forskningscirkel där konstruktiv dialog förts med representanter för bostadsförsörjningen och kommunala aktörer som representerar äldreomsorg",
+    "Lunds universitet", "Augusti", 2018, "April", 2020,"Beslutsstöd Bo Tillgängligt (BBT): Tvåårigt forskningsprojekt om förbättrad tillgänglighet i flerfamiljshus", "Lund", "Transkriberat, kodat och analyserat ljudinspelningar från forskningscirkelträffarna med hjälp av verktyget NVivo",
+)
+
+skills7 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Lunds universitet", "April", 2021, "Oktober", 2022,"Bostadsförsöket 2021: Ett medborgarforskningsprojekt om tillgängligheten i Sveriges bostäder", "Lund", "Fört dialog med intresseföreningar för seniorer (PRO, SPF) och personer med funktionshinder (Neuroförbundet) på lokal nivå för att få dem att engagera sig i forskningsprojektet",
+    "Lunds universitet", "April", 2021, "Oktober", 2022,"Bostadsförsöket 2021: Ett medborgarforskningsprojekt om tillgängligheten i Sveriges bostäder", "Lund", "Teknisk support och vägledning via telefon till projektdeltagare",
+    "Lunds universitet", "April", 2021, "Oktober", 2022,"Bostadsförsöket 2021: Ett medborgarforskningsprojekt om tillgängligheten i Sveriges bostäder", "Lund", "Under handledning men med stort mått av självständighet utformat digitala enkäter för studien med hjälp av verktyget REDCap samt analyserat dessa med hjälp av SPSS",
+)
+
+skills8 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Lunds universitet", "September", 2019, "December", 2020,"Doktorandrepresentant i institutionsstyrelsen (Hälsovetenskapliga institutionen)", "Lund", "- Deltagit i arbetet med att bevaka och driva frågor som rör forskarutbildning och frågor som rör arbetsrelaterade ekonomiska och sociala rättigheter",
+)
+
+skills9 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Lunds universitet", "February", 2019, "December", 2020,"Doktorandrepresentant i CASE:s styrgrupp", "Lund", "Deltagit i arbetet för att vidareutveckla CASE som ett starkt och uthålligt centrum för forskning om åldrande och stödjande miljöer vid Lunds universitet",
+    "Lunds universitet", "February", 2019, "December", 2020,"Doktorandrepresentant i CASE:s styrgrupp", "Lund", "Deltagit i beslutsfattandet kring om ytterligare forskningsmiljöer och forskare vid Lunds universitet ska anslutas",
+)
+
+skills10 <- tribble(
+    ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+    "Lunds universitet", "Januari", 2019, "December", 2020,"Representant för Hälsovetenskapliga institutionen i medicinska fakultetens doktorandråd (MDR)", "Lund", "Deltagit i arbetet med att bevaka och driva frågor som rör forskarutbildning och frågor som rör arbetsrelaterade ekonomiska och sociala rättigheter. ",
 )
 
 honors <- tribble(
@@ -65,7 +109,8 @@ edu2 <- tribble(
   ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
   "Lunds universitet", "Februari", 2024, "Pågående", NA,"Litteraturvetenskap: Litterära perspektiv på sjukdom och död (7,5 hp) ", "Online", "Kvällskurs på halvfart som ges på distans med stöd av den nätbaserade lärplattformen Canvas",
   "Lunds universitet", "Februari", 2024, "Pågående", NA,"Litteraturvetenskap: Litterära perspektiv på sjukdom och död (7,5 hp) ", "Online", "Diskussion av historiska och samtida litterära och teoretiska verk som aktualiserar och gestaltar människans möte med sjukdom och död",
-  "Blekinge tekniska högskola", "Augusti", 2016, "Juni", 2018, "Doktorandstudier i tillämpad hälsovetenskap", "Karlskrona", "Bla bla bla",
+  "Blekinge tekniska högskola", "Augusti", 2016, "Juni", 2018, "Doktorandstudier i tillämpad hälsovetenskap", "Karlskrona", "Två års forskningsstudier inom ramen för Horizon 2020-projektet SMART4MD som genomfördes i samverkan mellan fem europeiska länder",
+  "Blekinge tekniska högskola", "Augusti", 2016, "Juni", 2018, "Doktorandstudier i tillämpad hälsovetenskap", "Karlskrona", "Projektet gick ut på hur teknik i form av en hälsoapplikation på en surfplatta kan underlätta vardagen för äldre personer och deras närstående",
   "Linköpings universitet", "Augusti", 2014, "Juni", 2015, "Svenska som andraspråk 1 (30 hp)", "Linköping", "Studier av svenska språkets struktur (fonetik och grammatik), kulturfrågor, andraspråkutveckling, didaktik och andraspråksundervisning",
   "Linköpings universitet", "Augusti", 2005, "Juni", 2006, "Engelska A (30 hp)", "Linköping", "Ingående studier av det engelska språkets struktur, inklusive träning av muntlig och skriftlig språkfärdighet",
   "Linköpings universitet", "Januari", 2004, "April", 2004, "Teknik, kultur, samhälle (7,5 hp)", "Linköping/Online", "Introduktion till ett sociologiskt/kontextbundet synsätt på teknik och teknikutveckling",
@@ -73,7 +118,7 @@ edu2 <- tribble(
 
 edu3 <- tribble(
     ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
-    "Arrangerad av UserAge forskningsprogram med fokus på brukarinvolvering i forskning", NA, NA, "Maj", 2023,"Avslutningskonferens för forskningsprogrammet UserAge", "Lund", "Presentation (på engelska) av två delstudier i avhandlingen till internationell forskarpublik",
+    "Arrangerad av UserAge forskningsprogram med fokus på brukarinvolvering i forskning", NA, NA, "April", 2023,"Avslutningskonferens för forskningsprogrammet UserAge", "Lund", "Presentation (på engelska) av två delstudier i avhandlingen till internationell forskarpublik",
     "Arrangerad av UserAge forskningsprogram med fokus på brukarinvolvering i forskning", NA, NA, "Maj", 2023,"Avslutningskonferens för forskningsprogrammet UserAge", "Lund", "Presentation (på svenska) av två delstudier i avhandlingen till intresserad allmänhet",
     "Arrangerat av UserAge i samarbete med föreningen Vetenskap & Allmänhet", NA, NA, "Oktober", 2022, "Webbinarie om medborgarforskningsprojektet Bostadsförsöket*", "Stockholm/Online", "Redogörelse (på svenska) för resultatet i fjärde delstudien i avhandlingen till intresserad allmänhet",
     "Arrangerad av forskningscentret CASE (Centre for Ageing and Supportive Environments)", NA, NA, "Maj", 2019, "CASE-dagen", "Lund", "Presentation (på svenska) av andra delstudien i avhandlingen till intresserad allmänhet",
